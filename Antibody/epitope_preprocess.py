c55ecb7bc2f203a2edbdd92ec0642b66a8503313
Matthew P. Greving, Ph.D <biochemcompsci@gmail.com>
2021-06-20 15:11:10 -0700

    Load full dataset

--- Antibody/epitope_preprocess.py
+++ Antibody/epitope_preprocess.py
@@ -7,42 +7,52 @@ if __name__ == "__main__":
     # required to read Python 2 pickled data into Python 3
     dill._dill._reverse_typemap['ObjectType'] = object
 
-    epitope_data = "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_epipred/train.cpkl"
-
-    # encoding=bytes required to read Python 2 pickled data into Python 3
-    # data is a list of dictionaries describing Ab-Ag interactions with format described at https://github.com/vamships/PECAN
-    with open(epitope_data, "rb") as epitope_file:
-        epitopes_processed = cPickle.load(epitope_file, encoding="bytes")
+    epitope_data = ["/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_epipred/train.cpkl",
+                    "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_epipred/test.cpkl",
+                    "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_epipred/val.cpkl",
+                    "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_paratope/train.cpkl",
+                    "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_paratope/test.cpkl",
+                    "/home/biochemcompsci/datasets/epitope/pittala_context_aware_structure_ag_ab/data_paratope/val.cpkl"]
 
     # lookup table to map 1-hot AA vector encoding to single-letter code
-    aa_lookup_table = np.array(['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '*'])
+    aa_lookup_table = np.array(
+        ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '*'])
 
     # load labeled antigens into array
-    antigens = []
+    antigen_sequences = []
     labeled_epitopes = []
 
-    for idx in range(0, len(epitopes_processed) - 1):
-        if idx % 2 == 0:
-            # need str.encode for Python 2 to Python 3 interop
-            antigens.append(epitopes_processed[idx].get(str.encode('l_vertex')))
-            labeled_epitopes.append(epitopes_processed[idx].get(str.encode('label')))
+    for currFile in epitope_data:
 
-    # Generate sequences from one-hot vectors corresponding to each antigen
-    antigen_sequences = []
-    # loop through all antigens
-    for idx_a in range(0, len(antigens) - 1):
+        print("Loading data from file: " + str(currFile))
 
-        # loop through all sequence positions
-        curr_sequence = ""
+        # encoding=bytes required to read Python 2 pickled data into Python 3
+        # data is a list of dictionaries describing Ab-Ag interactions with format described at https://github.com/vamships/PECAN
+        epitopes_processed = {}
+        with open(currFile, "rb") as epitope_file:
+            epitopes_processed = cPickle.load(epitope_file, encoding="bytes")
 
-        for idx_b in range(0, len(antigens[idx_a]) - 1):
-            one_hot_aa_vec = np.array(antigens[idx_a][idx_b][0:21])
-            curr_sequence = curr_sequence + aa_lookup_table[one_hot_aa_vec.nonzero()][0]
+        for idx in range(0, len(epitopes_processed) - 1):
 
-        antigen_sequences.append(curr_sequence)
+            if idx % 2 == 0:
 
-    print(antigen_sequences)
-    print(labeled_epitopes)
+                # need str.encode for Python 2 to Python 3 interop
+                curr_antigen = epitopes_processed[idx].get(str.encode('l_vertex'))
+
+                # loop through all sequence positions
+                curr_sequence = ""
+                for idx_b in range(0, len(curr_antigen) - 1):
+
+                    one_hot_aa_vec = np.array(curr_antigen[idx_b][0:21])
+                    curr_sequence = curr_sequence + aa_lookup_table[one_hot_aa_vec.nonzero()][0]
+
+                antigen_sequences.append(curr_sequence)
+                labeled_epitopes.append(epitopes_processed[idx].get(str.encode('label'))[:, 1])
+                # print(curr_sequence)
+                # print(labeled_epitopes[len(labeled_epitopes) - 1])
+
+    print(len(antigen_sequences))
+    print(len(labeled_epitopes))
 
 
 
